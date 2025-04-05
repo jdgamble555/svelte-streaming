61f170de45784f6435fbc1e518ecea162cbcde86
Jonathan Gamble <jdgamble555@gmail.com>
2025-04-04 19:02:58 -0500

    first

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,19 @@
+import type { PageServerLoad } from "./$types";
+
+type Todo = {
+    title: string
+};
+
+const getTodo = async () => {
+    const randomTodo = Math.floor(Math.random() * 200) + 1;
+    return await fetch(`https://jsonplaceholder.typicode.com/todos/${randomTodo}`)
+        .then(r => r.json()) as Todo;
+};
+
+export const load: PageServerLoad = () => {
+    return {
+        streamed: {
+            todos: getTodo()
+        }
+    };
+};
